b68a8308a75f2799c3c9dfca6df9481115302be9
zerobruhman <onebruhmanpc@gmail.com>
2026-04-27 20:42:46 +0700

    menambah kan form dan beberapa security basic

--- README.md
+++ README.md
@@ -4,5 +4,22 @@
 - CREATE TABLE
 - INSERT DATA
 - SELECT QUERY
-- JOIN
 - UPDATE & DELETE
+---
+## Menambahkan Security basic
+- Menyembunyikan koneksi PHP > MySQL
+
+untuk file `koneksi.php` isinya kira-kira serti ini
+```php
+<?php
+$host = "localhost";
+$user = "root";
+$password = "passwordgwygy"; 
+$db   = "belajarmysql";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+```
--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "321", "belajarmysql");
-if (!$koneksi){
-    echo "Koneksi gagal";
-    return;
-}
+include "koneksi.php";
+echo "<script>console.log('Koneksi berhasil')</script>";
 $hasil = mysqli_query($koneksi, "SELECT * FROM Siswa");
-while ($baris = mysqli_fetch_assoc($hasil)){
-    echo "
-    Nama: {$baris["nama"]} 
-    Umur: {$baris["umur"]} 
-    Jurusan: {$baris["jurusan"]}
-    Kelas: {$baris["kelas"]} \n";    
-}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar MySQL</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Jurusan</th>
+            <th>Kelas</th>
+        </tr>
+        <?php while ($baris = mysqli_fetch_assoc($hasil)) { ?>
+        <tr>
+            <td><?= $baris["nama"]; ?></td>
+            <td><?= $baris["umur"]; ?></td>
+            <td><?= $baris["jurusan"]; ?></td>
+            <td><?= $baris["kelas"]; ?></td>
+        </tr>
+    <?php } ?>
+    </table>
+</body>
+</html>
